779f1cbc143139c56482f45ba409035fc435bfc8
vamsikrishna28 <sreegopalavamsi.veluvali@gmail.com>
2026-03-02 11:15:45 +0530

    utkarsh raj added UC5

---
+++ PalindromeApp/src/UC6.java
@@ -0,0 +1,40 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+    public class UC6 {
+
+        public static void main(String[] args) {
+
+
+            String input = "civic";
+
+
+            Queue<Character> queue = new LinkedList<>();
+
+
+            Stack<Character> stack = new Stack<>();
+
+
+            for (char c : input.toCharArray()) {
+                queue.add(c);      // Enqueue
+                stack.push(c);     // Push
+            }
+
+
+            boolean isPalindrome = true;
+
+
+            while (!queue.isEmpty()) {
+                if (queue.remove() != stack.pop()) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+
+            System.out.println("Input : " + input);
+            System.out.println("Is Palindrome? : " + isPalindrome);
+        }
+    }
